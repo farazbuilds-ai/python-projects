80dda5c182b806df09ad7a7fb75734f555faff9b
farazbuilds-ai <farazashraf1302@gmail.com>
2026-09-15 00:53:01 +0500

    Fix identation in fizzbuzz.py

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,11 +1,11 @@
 R=1
 while R<=50:
-   if R%3==0 and R%5==0:
-     print("FizzBuzz")
-   elif R%3==0: 
-     print("Fizz")
-   elif R%5==0:
-     print("Buzz")
-   else:
-     print(R)
-   R+=1
+    if R%3==0 and R%5==0:
+        print("FizzBuzz")
+    elif R%3==0: 
+        print("Fizz")
+    elif R%5==0:
+        print("Buzz")
+    else:
+        print(R)
+    R+=1
